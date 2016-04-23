4420011ca19664fa0be6bceb99646e53171e25dc
hi5 <hi5@users.noreply.github.com>
2016-04-23 09:54:08 +0200

    validate.cmd

---
+++ test/validate.cmd
@@ -0,0 +1,17 @@
+@echo off
+set err_level=0
+
+rem Loop over all ahk files in tests directory
+for /r %%i in (*.ahk) do (
+	start "testing" /B /wait "C:\Program Files\AutoHotkey\AutoHotkeyU32.exe" /iLib NUL /ErrorStdOut %%~nxi > testoutput.txt 2>&1
+	echo ** Running %%~nxi **
+	if errorlevel 1 (
+		echo *** Test file %%~nxi failed ***
+		set err_level=1
+	)
+	type testoutput.txt
+	echo.
+)
+
+rem EXIT SCRIPT
+exit /b %err_level%
